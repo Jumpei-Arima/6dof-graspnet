d0473ad32af1df4f834511d0adb957a9f4f056ee
arima <arijun0307@gmail.com>
2020-06-27 15:51:41 +0900

    update gripper_control_points dir path

--- tf_utils.py
+++ tf_utils.py
@@ -66,7 +66,7 @@ def get_control_point_tensor(batch_size, use_tf=True):
       Outputs a tensor of shape (batch_size x 6 x 3).
       use_tf: switches between outputing a tensor and outputing a numpy array.
     """
-    control_points = np.load('gripper_control_points/panda.npy')[:, :3]
+    control_points = np.load(os.path.dirname(__file__)+'/gripper_control_points/panda.npy')[:, :3]
     control_points = [[0, 0, 0], [0, 0, 0], control_points[0, :],
                       control_points[1, :], control_points[-2, :], control_points[-1, :]]
     control_points = np.asarray(control_points, dtype=np.float32)
